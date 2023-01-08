26f78838e813b6560aa4f5910ca894c8a7640dc8
Darian1996 <1934849492@qq.com>
2023-01-08 19:51:32 +0800

    2023-01-08 19:51:32 AutoCommit.sh by Darian

---
+++ sh/aliyun-about-zh-update-to-github.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# 复制过来
+cp /software/GitHub_Repositories/darian1996.github.io/_includes/about/zh.md /software/GitHub_Repositories/Darian1996
+# -f 强制覆盖
+mv -f /software/GitHub_Repositories/Darian1996/zh.md /software/GitHub_Repositories/Darian1996/README.md
+
+
+
+echo 'update Darian1996/README.md success'
+
