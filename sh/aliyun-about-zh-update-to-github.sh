18f7d2c739218f9e65073bcf589ffcee823c575b
Darian1996 <1934849492@qq.com>
2023-01-12 15:18:01 +0800

    2023-01-12 15:18:01 AutoCommit.sh by Darian

--- sh/aliyun-about-zh-update-to-github.sh
+++ sh/aliyun-about-zh-update-to-github.sh
@@ -6,6 +6,10 @@ cp /software/GitHub_Repositories/darian1996.github.io/_includes/about/zh.md /sof
 mv -f /software/GitHub_Repositories/Darian1996/zh.md /software/GitHub_Repositories/Darian1996/README.md
 
 
-
 echo 'update Darian1996/README.md success'
+
+# 读书的文章的覆盖
+\cp /software/GitHub_Repositories/darian-lucene-file/src/main/resources/static/other/will-found-books/index.html /software/GitHub_Repositories/darian1996.github.io/other/will-found-books
+
+echo 'update /other/will-found-books/indexhtml success'
 
